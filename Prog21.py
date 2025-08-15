3b05466d9718b1435d7bb67d2deca63faf684ba3
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-08-15 09:04:12 +0530

    Add files via upload

---
+++ Prog21.py
@@ -0,0 +1,8 @@
+Pizza_price=100
+puff_price=20
+cooldrink_price=10
+P_no=int(input("Enter the no of Pizza bought: "))
+Pu_no=int(input("Enter the no of Puffs bought: "))
+C_no=int(input("Enter the no of Cooldrink bought: "))
+total=(Pizza_price*P_no)+(puff_price*Pu_no)+(cooldrink_price*C_no)
+print(total)
