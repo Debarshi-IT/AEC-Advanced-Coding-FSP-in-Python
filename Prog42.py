b2e3e8b938abb1d2d304a4592cc8864cecb6d1ef
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-09-10 09:15:12 +0530

    Add files via upload

---
+++ Prog42.py
@@ -0,0 +1,11 @@
+# set the i-th bit of the number n
+#                   7654 3210
+# 25 = 16 + 8 + 1 = 0001 1001
+#          1 << 2 = 0000 0100 |
+#                   ---------
+#                   0001 1101 => 29
+def setBit(n, i):
+    return n | (1 << i)
+n = 25
+p = 2
+print(f"n = {n} and result = {setBit(n, p)}")
